905f1ab1c09d70a589b24cfe4e13514c7ce2cd9b
Manuel Rivas Gauna <manuelrivasgauna@cofarsur.com>
2023-06-25 09:54:56 -0300

    Agregue el evento de actualización de la página cada 20 seg y el contador regresivo  en el html de dogjoke

--- prueba_angular/src/app/dog-joke/dog-joke.component.ts
+++ prueba_angular/src/app/dog-joke/dog-joke.component.ts
@@ -11,14 +11,41 @@ export class DogJokeComponent {
   jokeSetup: string;
   imageData: any;
   jokePunchline: string;
+  private interval: any;
+  private interval_number: any;
+  regresiva: number;
 
   constructor(private jokeService: JokeService, private randomimgService: RandomimgService) {
     this.jokeSetup = '';
     this.imageData = null;
     this.jokePunchline = '';
+    this.regresiva = 20;
    }
   
   ngOnInit() {
+    this.load();
+
+    //Setear un intervalo de 20seg para ejecutar load() que genera las variables que se renderizan en la plantilla
+    this.interval = setInterval(() => {
+      this.load();    
+    }, 20000);
+
+    //Cuenta regresiva
+    this.interval_number = setInterval(() => {
+      this.regresiva -= 1;
+      if (this.regresiva === 0) {
+        this.regresiva = 20;
+      }
+    }, 1000);
+  }
+
+  //Detener interval cuando se destruye el componente
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    clearInterval(this.interval_number);
+  }
+
+  load(){
     this.jokeService.getJoke().subscribe(response => {
       this.jokeSetup = response.setup;
       this.jokePunchline = response.punchline;
